ad74ae0102ffb31c80ee708052af7b12d94c5684
John Williams <john@insidenewcity.com>
2019-12-08 18:26:50 -0500

    Still refining article display

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,15 @@ module.exports = {
   corePlugins: {
     fontFamily: false
   },
-  plugins: []
+  plugins: [
+    require("tailwindcss-responsive-embed")(),
+    require("tailwindcss-aspect-ratio")({
+      ratios: {
+        square: [1, 1],
+        "16/9": [16, 9],
+        "4/3": [4, 3],
+        "21/9": [21, 9]
+      }
+    }),  
+  ]
 }
